09a2f41efbdb4322de888be61cf378c10fc47159
Xeva266 <emmanuelarinze078@gmail.com>
2026-03-21 13:26:32 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,12 @@
 
+const cacheName = 'carvfit-v1';
+const staticAssets = ['./', './index.html', './manifest.json'];
+
+self.addEventListener('install', async e => {
+  const cache = await caches.open(cacheName);
+  await cache.addAll(staticAssets);
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
+});
